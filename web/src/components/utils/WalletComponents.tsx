5c715cbda3007fabe5a7563f33d6765a32eed0cb
Mike Solomon <mike.solomon@coinbase.com>
2024-12-23 18:24:12 -0500

    tw btn

--- web/src/components/utils/WalletComponents.tsx
+++ web/src/components/utils/WalletComponents.tsx
@@ -14,11 +14,58 @@ import {
   WalletDropdownDisconnect,
   WalletDropdownFundLink,
 } from '@coinbase/onchainkit/wallet';
-import { useAccount } from 'wagmi';
+import { createWallet, createWalletAdapter } from "thirdweb/wallets";
+import { ConnectButton, useActiveAccount, useActiveWalletChain, useSetActiveWallet } from "thirdweb/react";
+import { useConnect, useAccount, useDisconnect, useWalletClient, useSwitchChain } from "wagmi";
+import { viemAdapter } from "thirdweb/adapters/viem";
+import { useEffect, useState } from 'react';
+import { defineChain } from 'thirdweb/chains';
+import { CLIENT } from '~/constants';
+
 export function WalletComponents() {
   const { isConnected } = useAccount();
+  const { disconnectAsync } = useDisconnect();
+  const { switchChainAsync } = useSwitchChain();
+  const { data: walletClient } = useWalletClient();
+  const setActiveWallet = useSetActiveWallet();
+
   const connectButtonColor = isConnected ? "bg-gray-200" : "bg-blue-600 hover:bg-blue-700";
   const connectButtonTextSize = isConnected ? "text-md" : "text-lg";
+
+  useEffect(() => {
+    const setActive = async () => {
+      if (walletClient) {
+        // adapt the walletClient to a thirdweb account
+        const adaptedAccount = viemAdapter.walletClient.fromViem({
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+          walletClient: walletClient as any, // accounts for wagmi/viem version mismatches
+        });
+        // create the thirdweb wallet with the adapted account
+        const thirdwebWallet = createWalletAdapter({
+          client: CLIENT,
+          adaptedAccount,
+          chain: defineChain(await walletClient.getChainId()),
+          onDisconnect: async () => {
+            await disconnectAsync();
+          },
+          switchChain: async (chain) => {
+            await switchChainAsync({ chainId: chain.id as 8453 });
+          },
+        });
+        void setActiveWallet(thirdwebWallet);
+      }
+    };
+    void setActive();
+  }, [disconnectAsync, setActiveWallet, switchChainAsync, walletClient]);
+
+  if (isConnected) {
+    return (
+      <ConnectButton 
+        client={CLIENT} 
+        theme="light"
+      />
+    )
+  }
 
   return (
     <div className="flex justify-end">
