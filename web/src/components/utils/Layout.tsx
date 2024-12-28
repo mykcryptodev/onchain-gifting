1971ae06f60ca3b9c6b22b3ae4e180359f86acaf
Brandon Garvajal <brandon.garvajal@gmail.com>
2024-12-28 23:18:59 +0700

    fix: fix header logo and connect wallet

--- web/src/components/utils/Layout.tsx
+++ web/src/components/utils/Layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { useAccount } from "wagmi";
+import { WalletComponents } from "./WalletComponents";
 
 const Frame = dynamic(() => import("~/components/utils/Frame"), {
   ssr: false,
@@ -10,16 +11,12 @@ const Frame = dynamic(() => import("~/components/utils/Frame"), {
 export function Layout({ children }: { children: React.ReactNode }) {
   const { address } = useAccount();
   return (
-    <main className="flex min-h-screen flex-col items-center sm:px-20 sm:pb-20 sm:pt-10 p-4 w-full">
+    <main className="flex min-h-screen bg-gradient-to-br from-white to-blue-500 flex-col items-center sm:px-20 sm:pb-20 sm:pt-10 p-4 w-full">
       <div className="flex justify-between items-center w-full">
         <Link href="/">
-          <Image src="/images/logo.png" alt="Logo" width={32} height={32} priority />
+          <Image src="/images/base-logo.png" alt="Logo" width={32} height={32} priority />
         </Link>
-        {address && (
-          <Link href={`/from/${address}`}>
-            Gifts From Me
-          </Link>
-        )}
+        <WalletComponents />
       </div>
       {children}
       <Frame />
