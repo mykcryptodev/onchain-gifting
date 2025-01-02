72f274bbbfd01da28f0390d685446366147d8e21
Mike Solomon <mike.solomon@coinbase.com>
2025-01-02 12:05:00 -0500

    allow custom erc20s

--- web/src/server/api/routers/token.ts
+++ web/src/server/api/routers/token.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { CHAIN, UNKNOWN_TOKEN_IMAGE } from "~/constants";
+import { CHAIN, UNKNOWN_TOKEN_IMAGE, CLIENT } from "~/constants";
 import { base } from "thirdweb/chains";
 import { getChainMetadata } from "thirdweb/chains";
-import { NATIVE_TOKEN_ADDRESS } from "thirdweb";
+import { NATIVE_TOKEN_ADDRESS, getContract, readContract, toTokens } from "thirdweb";
 import { customTokenList } from "~/constants/token_lists/custon";
+import { type ZapperTokenBalance } from "~/types/zapper";
+
 export const tokenRouter = createTRPCRouter({
   getImage: publicProcedure
     .input(z.object({ tokenAddress: z.string() }))
@@ -31,6 +33,80 @@ export const tokenRouter = createTRPCRouter({
       if (!chainName) throw new Error(`Chain ${CHAIN.id} not supported by coingecko`);
       const res = await fetch(`https://api.coingecko.com/api/v3/coins/${chainName}/contract/${input.tokenAddress}`);
       const json = (await res.json()) as { image: { large: string } };
-      return json.image.large ?? UNKNOWN_TOKEN_IMAGE;
+      return json.image?.large ?? UNKNOWN_TOKEN_IMAGE;
+    }),
+
+  getCustomToken: publicProcedure
+    .input(z.object({ 
+      tokenAddress: z.string(),
+      userAddress: z.string(),
+    }))
+    .query(async ({ input }) => {
+      const contract = getContract({
+        client: CLIENT,
+        chain: CHAIN,
+        address: input.tokenAddress,
+      });
+
+      const [name, symbol, decimals, balance] = await Promise.all([
+        readContract({
+          contract,
+          method: [
+            "0x06fdde03",
+            [],
+            [{ internalType: "string", name: "", type: "string" }]
+          ],
+          params: []
+        }),
+        readContract({
+          contract,
+          method: [
+            "0x95d89b41",
+            [],
+            [{ internalType: "string", name: "", type: "string" }]
+          ],
+          params: []
+        }),
+        readContract({
+          contract,
+          method: [
+            "0x313ce567",
+            [],
+            [{ internalType: "uint8", name: "", type: "uint8" }]
+          ],
+          params: []
+        }),
+        readContract({
+          contract,
+          method: [
+            "0x70a08231",
+            [{ internalType: "address", name: "account", type: "address" }],
+            [{ internalType: "uint256", name: "", type: "uint256" }]
+          ],
+          params: [input.userAddress]
+        })
+      ]);
+
+      const imageUrl = await tokenRouter.createCaller({}).getImage({ tokenAddress: input.tokenAddress });
+      const chainName = CHAIN.name ?? "Base";
+
+      const customToken: ZapperTokenBalance = {
+        address: input.tokenAddress,
+        network: chainName,
+        token: {
+          balance: toTokens(balance, Number(decimals)),
+          balanceUSD: 0,
+          baseToken: {
+            address: input.tokenAddress,
+            decimals: Number(decimals),
+            imgUrl: imageUrl,
+            name,
+            symbol,
+            network: chainName,
+          },
+        },
+      };
+
+      return customToken;
     }),
 });
